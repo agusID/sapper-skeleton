01e516766f0cdf807b443b5fad2a692df745b435
Dwi Agustianto <dwi.agustianto@ralali.com>
2019-10-21 13:45:51 +0700

    resolve polka error module

--- src/components/Button/Button.test.js
+++ src/components/Button/Button.test.js
@@ -1,10 +1,10 @@
 import { render, cleanup } from '@testing-library/svelte'
 import Button from './Button.svelte'
 
-const renderButton = props => {
-  const { container } = render(Button, { props })
-  return container
-}
+// const renderButton = props => {
+//   const { container } = render(Button, { props })
+//   return container
+// }
 
 beforeEach(cleanup)
 
@@ -13,11 +13,16 @@ afterEach(() => {
 })
 
 describe('Button', () => {
-  test('should render text and default color', () => {
-    const container = renderButton({ children: 'Hello world!' })
-    const button = container.querySelector('.btn')
-    console.log(button)
-    // expect(button.innerHTML).toBe('Hello world!')
-    // expect(button.className).toBe('btn btn-secondary')
+  // test('should render text and default color', () => {
+  //   const container = renderButton({ children: 'Hello world!' })
+  //   const button = container.querySelector('.btn')
+  //   expect(button.innerHTML).toBe('Hello world!')
+  //   expect(button.className).toBe('btn btn-secondary')
+  // })
+  test('shows proper heading when rendered', () => {
+    const { getByText } = render(Button, {
+      props: { children: 'Button Text' },
+    })
+    expect(getByText('Button Text')).toBeInTheDocument()
   })
 })
